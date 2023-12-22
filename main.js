404ef95f2046891b0921cd097dd7e37ab01ba731
RenatoFringuello <renatofringuello@hotmail.it>
2023-12-23 00:36:57 +0100

    add the select to-print, print in another page

--- main.js
+++ main.js
@@ -1,47 +1,131 @@
 // players
-const players = [
-    'Rhysand', 
-    'Feyre', 
-    'Suriel', 
-    'Nesta', 
-    'Tamlin', 
-    'Elain', 
-    'Lucien', 
-    'Gwyn', 
-    'Cassian', 
-    'Azriel', 
-    'Ruhn', 
-    'Hunt', 
-    'Aelin', 
-    'Rowan', 
-    'Lysandra', 
-    'Lidia', 
-    'Danika', 
-    'Bryce', 
-    'Manon', 
-    'Dorian', 
-    'Chaol'
+let players    = [
+    "Rhysand", 
+    "Feyre", 
+    "Suriel", 
+    "Nesta", 
+    "Tamlin", 
+    "Elain", 
+    "Lucien", 
+    "Gwyn", 
+    "Cassian", 
+    "Azriel", 
+    "Ruhn", 
+    "Hunt", 
+    "Aelin", 
+    "Rowan", 
+    "Lysandra", 
+    "Lidia", 
+    "Danika", 
+    "Bryce", 
+    "Manon", 
+    "Dorian", 
+    "Chaol"
 ]
+// let is_loading = true
 
 // DOM elements
-const paper     = document.getElementById('paper')
-const test_cb   = document.getElementById('test-checkbox')
-const print_btn = document.getElementById('header-collapse')
+const paper      = document.getElementById('paper')
+const print_btn  = document.getElementById('print')
+const test_cb    = document.getElementById('test-checkbox')
+const to_print   = document.getElementById('to-print')
+test_cb.checked  = false
 
-//listen to the btn
-print_btn.addEventListener('click', (e)=>{
-    // remove the header
-    const header = document.getElementById('header')
-    header.remove()
+// fn
+// function showLoader(){
+// const loader = `
+// <div class="loader-container">
+//     <div class="loader"></div>
+// </div>`
 
-    // print
-    window.print()
-})
+// paper.innerHTML = loader
+// }
 
-// listen to the test checkbox
-test_cb.addEventListener('change', (e)=>{
+function loadPage(){
+    setSelectToPrint(test_cb.checked, to_print)
+    showSet(test_cb.checked)
+}
+
+// function getPlayers(){
+//     is_loading = true
+//     fetch(`http://127.0.0.1:5500/players.json`)
+//         .then(response => response.json())
+//         .then(data     => {
+//             players = data;
+//             is_loading = false
+//             loadPage()
+//         })
+//         .catch(error => {
+//             is_loading = false
+//             console.error('Error fetching image names:', error);
+//         });
+// }
+
+function setSelectToPrint(is_test, to_print){
+    let appendHTML = `<option value="" selected disabled>-- select the cards to print --</option>`
+    if(is_test){
+        // append team, guess, all
+        appendHTML += `
+        <option value="0">ALL</option>>
+        <option value="1">TEAM 1</option>
+        <option value="2">GUESS</option>`
+    }
+    else{
+        appendHTML += `
+        <option value="0">ALL</option>>
+        <option value="1">TEAM 1</option>
+        <option value="2">TEAM 2</option>
+        <option value="3">GUESS</option>`
+    }
+    to_print.innerHTML = appendHTML 
+}
+
+function printSet(index){
+    // se non viene selezionato nulla allora stampa tutto (3 = ALL)
+    index = (index === '') ? 0 : parseInt(index, 10)
+
+    // Create a new window or iframe
+    const printWindow = window.open('', '_blank');
+
+    // Add the HTML content you want to print
+    let teams = document.querySelectorAll('.team');
+
+    // metto in un array il singolo team in modo da implementare un solo tipo di inject tramite foreach (per il caso in cui sia ALL)
+    teams = (index !== 0) ? [teams[index -1]] : teams
+
+    let contentToPrint = ''
+
+    teams.forEach((team)=>{
+        contentToPrint += team.outerHTML 
+    })
+
+    printWindow.document.write(`
+      <html>
+        <head>
+            <link rel="stylesheet" href="style.css">
+            <title>Print</title>
+        </head>
+        <body>
+            ${contentToPrint}
+        </body>
+      </html>
+    `);
+  
+    // to load the css
+    window.setTimeout(function(){
+        // Close the document
+        printWindow.document.close();
+      
+        // Print the content
+        printWindow.print();
+
+        window.clearTimeout(this)
+    }, 2000)
+}
+
+function showSet(is_test){
     paper.innerHTML = ``
-    if (e.srcElement.checked){
+    if (is_test){
         paper.innerHTML = `
         <div class="team">
             <div class="front-back-wrapper">
@@ -78,11 +162,12 @@ test_cb.addEventListener('change', (e)=>{
         // 3 teams
         for (let i = 0; i < 3; i++) {
             // get the team type
-            team_type      = (i == 0) ? 'player1'    : (i == 1) ? 'player2' : 'guess'
+            team_type      = (i == 0) ? 'team_1'    : (i == 1) ? 'team_2' : 'guess'
             team_back_type = (i  > 1) ? 'guess_back' : team_type
             card_size      = (i  > 1) ? 'big'        : 'small'
 
-            
+            // inject team name
+            paper.innerHTML += `<div class="team_name">${team_type.replace('_', ' ')}</div>`
             
             // init the team
             const team  = document.createElement('div')
@@ -113,4 +198,18 @@ test_cb.addEventListener('change', (e)=>{
             paper.appendChild(team)
         }
     }
+}
+
+//listen to the btn
+print_btn.addEventListener('click', (e)=>{
+    const to_print   = document.getElementById('to-print')
+    printSet(to_print.value)
 })
+
+// listen to the test checkbox
+test_cb.addEventListener('change', (e)=>{
+    loadPage()
+})
+
+// init 
+loadPage()
